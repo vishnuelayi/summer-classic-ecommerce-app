2011175a91f017d298dba5fe4aad6582481c886e
Vishnu Elayi <vishnuelayi@gmail.com>
2023-11-13 00:57:08 +0530

    added a filter and a sort section in CategoryPage componenent

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -10,6 +10,36 @@ const CategoryPage = () => {
     <div>
       <Announce />
       <Navbar />
+      <div className=" flex-col flex p-5">
+        <h1 className=" text-[30px]">Men's Cloth</h1>
+        <div className=" flex items-center justify-between mt-3">
+            <div className="flex">
+                <p>Filter by</p>
+                <select className=" ml-3 border-silver border-2">
+                    <option selected disabled>Size</option>
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+
+                <select className=" ml-3 border-silver border-2">
+                    <option selected disabled>Color</option>
+                    <option>Yellow</option>
+                    <option>Red</option>
+                    <option>Black</option>
+                </select>
+            </div>
+            <div className=" flex">
+              <p>Sort By</p>
+              <select  className=" ml-3 border-silver border-2">
+                <option>Newest (first)</option>
+                <option>Oldest (first)</option>
+                <option>Price (Asc)</option>
+                <option>Price (Des)</option>
+              </select>
+            </div>
+        </div>
+      </div>
       <Products />
       <NewsLetter />
       <Footer />
